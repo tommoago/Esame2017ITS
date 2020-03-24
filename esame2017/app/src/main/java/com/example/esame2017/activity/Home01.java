8704df3e4d78495e529a4171ae5054b8bb7e2974
tommo.ago <tommo.ago@gmail.com>
2020-03-24 10:52:24 +0100

    primo commit

---
+++ esame2017/app/src/main/java/com/example/esame2017/activity/Home01.java
@@ -0,0 +1,16 @@
+package com.example.esame2017.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.esame2017.R;
+
+public class Home01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home01);
+    }
+}
---
+++ esame2017/app/src/main/java/com/example/esame2017/activity/Ord01.java
@@ -0,0 +1,18 @@
+package com.example.esame2017.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.esame2017.R;
+
+public class Ord01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ord01);
+        getSupportActionBar().setTitle("Gestione Ordini");
+
+    }
+}
