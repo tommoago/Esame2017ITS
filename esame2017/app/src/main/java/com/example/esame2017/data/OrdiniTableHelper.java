b4fce7889b7d9eed1a5285e4b2cac016164ad074
tommo.ago <tommo.ago@gmail.com>
2020-03-24 11:46:26 +0100

    classe table helper

---
+++ esame2017/app/src/main/java/com/example/esame2017/data/OrdiniTableHelper.java
@@ -0,0 +1,15 @@
+package com.example.esame2017.data;
+
+import android.provider.BaseColumns;
+
+public class OrdiniTableHelper implements BaseColumns {
+
+    public static final String TABLE_NAME="ordini";
+    public static final String DATA="data";
+    public static final String NUM_PIZZE="numPizze";
+    public static final String NUM_PANINI="numPanini";
+    public static final String NUM_BIBITE="numBibite";
+    public static final String NUM_GELATI="numGelati";
+    public static final String NUM_CAFFE="numCaffe";
+
+}
