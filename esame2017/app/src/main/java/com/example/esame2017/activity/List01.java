59d1acaecaaa3a8bc006fc178d888b286373d6ea
tommo.ago <tommo.ago@gmail.com>
2020-03-25 11:00:21 +0100

    creata activity list01 con layout

---
+++ esame2017/app/src/main/java/com/example/esame2017/activity/List01.java
@@ -0,0 +1,16 @@
+package com.example.esame2017.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.esame2017.R;
+
+public class List01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list01);
+    }
+}
