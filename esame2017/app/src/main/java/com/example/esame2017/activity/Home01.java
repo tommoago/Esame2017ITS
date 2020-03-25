db7aac5b3ae665ae123033a1e62d5d7cd499788a
tommo.ago <tommo.ago@gmail.com>
2020-03-25 09:53:50 +0100

    logica ORD01

--- esame2017/app/src/main/java/com/example/esame2017/activity/Home01.java
+++ esame2017/app/src/main/java/com/example/esame2017/activity/Home01.java
@@ -2,15 +2,27 @@ package com.example.esame2017.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.esame2017.R;
 
 public class Home01 extends AppCompatActivity {
+    Button mInsOrd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home01);
+
+        mInsOrd = findViewById(R.id.buttonInsertOrd);
+        mInsOrd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home01.this, Ord01.class));
+            }
+        });
     }
 }
--- esame2017/app/src/main/java/com/example/esame2017/activity/Ord01.java
+++ esame2017/app/src/main/java/com/example/esame2017/activity/Ord01.java
@@ -3,18 +3,24 @@ package com.example.esame2017.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.QuickContactBadge;
 import android.widget.TextView;
 
 import com.example.esame2017.R;
+
+import java.util.Calendar;
 
 public class Ord01 extends AppCompatActivity {
     Button mAnnulla, mConferma;
     TextView mData, mPizzaCT, mPaninoCT, mBibitaCT, mGelatoCT, mCaffeCT;
     LinearLayout mPizzaL, mPaninoL, mBibitaL, mGelatoL, mCaffeL; //Counter
 
-    int mPizzaC=0, mPaninoC=0, mBibitaC=0, mGelatoC=0, mCaffeC=0;
+    int mPizzaC = 0, mPaninoC = 0, mBibitaC = 0, mGelatoC = 0, mCaffeC = 0;
+
+    String mDataT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,65 @@ public class Ord01 extends AppCompatActivity {
         setContentView(R.layout.activity_ord01);
         getSupportActionBar().setTitle("Gestione Ordini");
 
+        setViews();
 
+        mDataT = Calendar.getInstance().toString();
+        mData.setText(mDataT);
+
+        mPizzaL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPizzaC++;
+                mPizzaCT.setText(mPizzaC + "");
+            }
+        });
+
+        mPaninoL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPaninoC++;
+                mPaninoCT.setText(mPaninoC + "");
+            }
+        });
+
+        mBibitaL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBibitaC++;
+                mBibitaCT.setText(mBibitaC + "");
+            }
+        });
+
+        mGelatoL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGelatoC++;
+                mGelatoCT.setText(mGelatoC + "");
+            }
+        });
+
+        mCaffeL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCaffeC++;
+                mCaffeCT.setText(mCaffeC + "");
+            }
+        });
+    }
+
+    private void setViews() {
+        mAnnulla = findViewById(R.id.buttonAnnulla);
+        mConferma = findViewById(R.id.buttonConferma);
+        mData = findViewById(R.id.textViewData);
+        mPaninoCT = findViewById(R.id.paninoCounter);
+        mPizzaCT = findViewById(R.id.pizzaCounter);
+        mGelatoCT = findViewById(R.id.gelatoCounter);
+        mBibitaCT = findViewById(R.id.bibitaCounter);
+        mCaffeCT = findViewById(R.id.caffeCounter);
+        mPizzaL = findViewById(R.id.pizzaLayout);
+        mPaninoL = findViewById(R.id.paninoLayout);
+        mBibitaL = findViewById(R.id.bibitaLayout);
+        mGelatoL = findViewById(R.id.gelatoLayot);
+        mCaffeL = findViewById(R.id.caffeLayout);
     }
 }
